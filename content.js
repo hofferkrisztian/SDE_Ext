a8618bce2e80bbec0d8fca54714ee06fefe58ce4
hofferkrisztian <hoffer.krisztian@gmail.com>
2021-01-19 11:29:23 +0100

    first

---
+++ content.js
@@ -0,0 +1,29 @@
+$(".plex-page-title").text("My SQL Development Environment ;)");    
+document.onkeydown = capturekey;
+document.onkeypress = capturekey;
+document.onkeyup = capturekey;
+function capturekey(e) {
+    e = e || window.event;    
+    if (e.code == 'F4') {
+        e.preventDefault()
+        e.stopPropagation()                
+        $("li:contains('View Stored Procedures')").click()        
+    }
+    if (e.code == 'F5') {
+        e.preventDefault()
+        e.stopPropagation()        
+        $("li:contains('Execute')").click()        
+    }    
+    if (e.code == 'F6') {
+        e.preventDefault()
+        e.stopPropagation()                
+        $("li:contains('Save Stored Procedure')").click()
+    }                                    
+    if (e.key === 'Escape') {
+        e.preventDefault()
+        $(".plex-banner-close").click()
+    }   
+    if (e.ctrlKey && e.key == 'q') {       
+       $("ul#QueryTabList > li.active").children().find("span.Remove").click()
+    } 
+}
